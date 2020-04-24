6a8e717e6a3d79b9d19cee9b0e094ceb5c66eab6
kevin <kev.ubedo@gmail.com>
2020-04-23 21:54:42 -0600

    Permisos

--- go-docker/interface.sh
+++ go-docker/interface.sh
@@ -1,5 +1,5 @@
-ip link add mac0 link enp0s3 type macvlan mode bridge
-ip addr add 172.20.4.9/28 dev mac0
-ifconfig enp0s3 down
-ifconfig enp0s3 up promisc
-ifconfig mac0 up
+sudo ip link add mac0 link enp0s3 type macvlan mode bridge
+sudo ip addr add 172.20.4.9/28 dev mac0
+sudo ifconfig enp0s3 down
+sudo ifconfig enp0s3 up promisc
+sudo ifconfig mac0 up
